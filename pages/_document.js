201e585095c188ce7add19655475ae0e8848cd56
Xu Lei <alivelee6@gmail.com>
2017-10-11 16:45:47 +0800

    add styled component configuration

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Head, Main, NextScript } from 'next/document'
 import flush from 'styled-jsx/server'
+import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
@@ -9,16 +10,21 @@ export default class MyDocument extends Document {
   }
 
   render() {
+    const sheet = new ServerStyleSheet()
+    const main = sheet.collectStyles(<Main />)
+    const styleTags = sheet.getStyleElement()
     return (
       <html lang="en">
         <Head>
           <title>NextTech</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="stylesheet" href="/static/normalize.css" />
+          {styleTags}
         </Head>
         <body>
-          <Main />
+          {main}
           <NextScript />
+          <script src="https://use.fontawesome.com/a8fab5f238.js"></script>
         </body>
       </html>
     )
