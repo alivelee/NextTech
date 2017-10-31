3ef84e4fa400b9c188561713249881288bf68dee
Xu Lei <a871460434@gmail.com>
2017-10-31 20:43:05 +0800

    complete the desktop version nav

--- component/NavBar.js
+++ component/NavBar.js
@@ -1,38 +1,42 @@
 import React from 'react';
 import styled from 'styled-components';
 const Nav = styled.nav`
-  position: sticky;
-  top: 15px;
-  padding: 10px;
-  background-color: #1E1E1E;
-  color: white;
-  width: 100px;
-  font-size: 0.16rem;
+  display: block;
+  position: fixed;
+  width: 100vw;
+  top: 0;
+  font-size: 0.4rem;
+  padding: 0.2rem 0 0.3rem 0.2rem;
+  background: #eee;
+  height: 1.3rem;
+  z-index: 1;
 `;
 const Ul = styled.ul`
   margin: 0;
   padding: 0;
 `;
 const Navlist = styled.li`
-  display: block;
+  display: inline;
   list-style: none;
   height: 40px;
   line-height: 40px;
-  margin: 20px 0;
+  margin: 20px;
+  &:first-child {
+    margin-left: 0;
+  }
 `;
 const Link = styled.a`
-  display: inline-block;
   text-decoration: none;
   padding-left: 5px;
-  color: ${props => props.active ? 'red' : 'white'};
+  color: ${props => props.active ? 'red' : 'black'};
 `;
 const NavBar = (props) => (
   <Nav>
     <Ul>
-      <Navlist><Link href='/topics' active={props.fromType === 'topics'}>topics</Link></Navlist>
-      <Navlist><Link href='/collections' active={props.fromType === 'collections'}>collections</Link></Navlist>
-      <Navlist><Link href='/posts' active={props.fromType === 'posts'}>posts</Link></Navlist>
-      <Navlist><Link href='/user' active={props.fromType === 'user'}>user</Link></Navlist>
+      <Navlist><Link href='/topics' active={props.fromType === 'topics'}>Topics</Link></Navlist>
+      <Navlist><Link href='/collections' active={props.fromType === 'collections'}>Collections</Link></Navlist>
+      <Navlist><Link href='/posts' active={props.fromType === 'posts'}>Posts</Link></Navlist>
+      <Navlist><Link href='/user' active={props.fromType === 'user'}>User</Link></Navlist>
       {/* <Navlist><Link href='/comments' active={props.fromType === 'comments'}>comments</Link></Navlist> */}
     </Ul>
   </Nav>
--- styles/components/Container.js
+++ styles/components/Container.js
@@ -3,4 +3,5 @@ export const Container = styled.section`
 	width: 100vw;
 	height: 100vh;
 	position: relative;
+	margin-top: 1.3rem;
 `;
